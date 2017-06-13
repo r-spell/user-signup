d04d58588017853982c94fb7a4e947e6d8efbc64
Rosanna Speller <rosanna.speller@gmail.com>
2017-06-13 16:03:29 -0500

    updated email validation

--- main.py
+++ main.py
@@ -13,16 +13,25 @@ def string_invalid(string):
     if len(string)<3 or len(string)>20 or ' ' in string:
         return True
     return False
-    
+
+def email_invalid(string):
+    if string_invalid(string):
+        return True
+    if string.count('@') != 1 or string.count('@') != 1 :
+        return True
+    return False
+
 @app.route("/", methods=['POST'])
 def validate_form():
 
     username = request.form['username']
     password = request.form['password']
     verify = request.form['verify']
+    email = request.form['email']
     username_error = ''
     password_error = ''
     verify_error = ''
+    email_error = ''
 
 
     if string_invalid(username):
@@ -31,12 +40,17 @@ def validate_form():
 
     if string_invalid(password):
         password_error = "That's not a valid password"
-        
+        password = ''
+
     if len(password_error) != 0 or verify != password:
         verify_error = "Passwords do not match"
         password = ''
         verify = ''
     
-    return render_template('signup_form.html', username_error=username_error, password_error=password_error,verify_error=verify_error)
+    if email != '':
+        if email_invalid(email):
+            email_error = "That's not a valid email"
+
+    return render_template('signup_form.html', username_error=username_error, password_error=password_error,verify_error=verify_error, email_error=email_error)
 
 app.run()
