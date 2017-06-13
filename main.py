398cf461306376e3dfbb56a990f0c4b4bf1ab0a0
Rosanna Speller <rosanna.speller@gmail.com>
2017-06-13 15:01:03 -0500

    add errors for blank inputs

--- main.py
+++ main.py
@@ -9,4 +9,28 @@ app.config['DEBUG'] = True
 def index():
     return render_template('signup_form.html')
 
+@app.route("/", methods=['POST'])
+def validate_form():
+
+    username = request.form['username']
+    password = request.form['password']
+    verify = request.form['verify']
+    username_error = ''
+    password_error = ''
+    verify_error = ''
+
+
+    if len(username)==0:
+        username_error = "That's not a valid username"
+    
+    if len(password)==0:
+        password_error = "That's not a valid password"
+        
+    if len(verify)==0 or verify != password:
+        verify_error = "Passwords do not match"
+        password = ''
+        verify = ''
+    
+    return render_template('signup_form.html', username_error=username_error, password_error=password_error,verify_error=verify_error)
+
 app.run()
